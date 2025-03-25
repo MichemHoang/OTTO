633a71d2ab82812ac9b1ebb507a037a7410a3bfd
MichemHoang <hoang999.matrix@gmail.com>
2025-03-25 08:02:48 +0100

    indentation and stuff

--- README.md
+++ README.md
@@ -1,16 +1,24 @@
 Simple chess engine in c++
 doesnt support UCI interface (yet)
 Require c++ 14 and up (for gtest)
+goal is 2k5 elo (for now)
 
-## _Desired feature (in the work)
+## Desired feature (in the work)
 
 - **Parallelization Support:**
-  - **Move Generation and Alpha-Beta Searching:** generate legal moves and explore multiple branches of the game tree in parallel to improve search depth and speed. Using openMP
+  - **Move Generation and Alpha-Beta Searching:** generate legal moves and explore multiple branches of the game tree in parallel to improve search depth and speed. Using openMP.
 
 - **Monte-Carlo Tree Search (MCTS):**
   - Integrate MCTS to handle positions with a high branching factor that traditional search methods might struggle with.
 
 - **Thorough Testing and Benchmarking:**
+  - Late game need more testing for sure
+
+- **Handling opening book:** 
+  - not in immediate future 
+
+
+
 
 
 
--- test/benchmark.cpp
+++ test/benchmark.cpp
@@ -15,7 +15,7 @@ void BENCHMARK(Func func, const std::string& funcName, int loop, Args... args) {
     for (int i=0; i < loop; i++){
         //check for callable?? maybe use std::invocable?
         //what if i put in some bs? what happen at run time?
-                                                                                        func(std::forward<Args>(args)...);
+        func(std::forward<Args>(args)...);
     }
 
     auto end = std::chrono::high_resolution_clock::now();
@@ -27,6 +27,7 @@ int main(){
     BOARD test1;
     BOARD_C test;
     std::string FENFEN = "4rb2/3qrk2/1p1p1n2/7p/P2P4/4R2P/1BQN1P2/1K4R1 w - - 3 39";
+    ExtMove MoveList[MAX_MOVES];
     //std::string FENFEN = STANDARD;
     FEN_Op::READ_FEN(FENFEN, &test1);
     test.ReadFENString(FENFEN);
@@ -35,7 +36,6 @@ int main(){
     //std::vector<ExtMove> AAA = GENERATE::AllMoves(test, test.Side_to_move);
     test.PrintAllMove();
     BENCHMARK(GENERATE::AllMoves, "GENERATE::AllMoves", 200000, test, test.Side_to_move);
-	ExtMove MoveList[MAX_MOVES];
     BENCHMARK(GENERATE::AllMove, "GENERATE::AllMove", 200000, test1, MoveList, test1.Side_to_move);
     BENCHMARK(GENERATE::AllMoves_MP, "GENERATE::AllMoves_MP", 200000, test, test.Side_to_move);
 }
